38d8bae9dc1c181881515fa1e4ac3809fa8d12ec
Cheryl H <cheryl.huber@my.csun.edu>
2015-01-28 23:38:49 -0800

    First Commit

---
+++ src/CarpetDriver.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cheryl
+ */
+import java.util.Scanner;
+
+public class CarpetDriver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double l,w;
+        System.out.println("Please enter room dimensions in feet.");
+        System.out.print("Enter length:");
+        l = in.nextDouble();
+        System.out.print("Enter width:");
+        w = in.nextDouble();
+        
+        RoomDimension myRoom = new RoomDimension(l,w);
+        RoomCarpet myCarpet = new RoomCarpet(myRoom,8.00);//$8 per sq foot
+        System.out.print("The total cost of a "+myRoom+" sqft room would be $");
+        System.out.println(myCarpet);
+    }
+    
+}
